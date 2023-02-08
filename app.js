e54e57163fb9df5431fc015819197d8b9278302e
miguelzuluag <zuluagaherreramiguelangel@gmail.com>
2023-02-07 19:10:52 -0500

    Cambios importantes

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"));
 })
 
+
 app.get("/register.html", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/register.html"));
 })
